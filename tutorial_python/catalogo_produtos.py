e98ab92f20e322720d597eac72382c6c1f028a16
Alexandre Clemente <aleclemente@gmail.com>
2026-08-31 20:03:49 -0300

    fix: code fix

--- tutorial_python/catalogo_produtos.py
+++ tutorial_python/catalogo_produtos.py
@@ -7,7 +7,7 @@ def exibir_catalogo(cat):
  print(f"{"Produto":<12} {"Preço":>10} {"Estoque":>9}")
  print("-" * 34)
  for produto, info in cat.items():
- print(f"{produto:<12} R${info["preco"]:>8.2f} {info["estoque"]:>9}")
+  print(f"{produto:<12} R${info['preco']:>8.2f} {info['estoque']:>9}")
 exibir_catalogo(catalogo)
 # Total em estoque
 total = sum(i["preco"] * i["estoque"] for i in catalogo.values())
